fa1a84d495675b966f5f11440dbeebae3ba2aa55
WangShuai <abc_billy@163.com>
2020-07-21 20:22:11 +0800

    初始化项目

---
+++ src/main/resources/db/migration/V1.0.0.0__init.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE `user` (
+  `id` VARCHAR(40) NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `phone` VARCHAR(20) NULL,
+  `realName` VARCHAR(40) NULL,
+  `password` VARCHAR(40) NULL,
+  PRIMARY KEY (`id`));
