79153ba2f684a8f61becae1a0c16612bb9ed3f93
WangShuai <abc_billy@163.com>
2020-07-22 16:14:57 +0800

    增加了接口测试的基础类

---
+++ src/test/java/com/xiangyun/tdd/BaseTest.java
@@ -0,0 +1,31 @@
+package com.xiangyun.tdd;
+
+import com.xiangyun.Application;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.sql.SQLException;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT, classes = Application.class)
+@ExtendWith(SpringExtension.class)
+public abstract class BaseTest {
+
+    @LocalServerPort
+    protected int port;
+
+    @BeforeEach
+    public void Before() throws SQLException {
+        RestAssured.port = port;
+//        RestAssured.basePath = "/";
+
+    }
+
+}
+
+
